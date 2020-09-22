72eaac59731eb4c18a775d69d8caebc631edfbd7
InsertIntoHTL <5125@htl.rennweg.at>
2020-09-22 16:05:08 +0200

    hehe

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,29 @@
 import { Template } from 'meteor/templating';
 
 import { FeaturedGifs } from '../api/collections.js';
+import { Results } from '../api/collections.js';
 
 import './body.html';
 
 Template.body.helpers({
     featuredGifs() {
         // Show newest tasks at the top
-        return FeaturedGifs.find({}, {});
+        return FeaturedGifs.find({});
     },
+    results() {
+        // Show newest tasks at the top
+        return Results.find({}, {});
+    }
 });
+
+Template.body.events({
+    'submit .new-search'(event) {
+        event.preventDefault();
+        const target = event.target;
+        const text = target.text.value;
+
+        Meteor.call("search", text);
+
+        target.text.value = "";
+    }
+})
